670f7806de92e435ff614cda0394a189d26d8b13
CoolGuy <admin@coolguy.pw>
2016-02-23 20:03:54 +0800

    实现部分逻辑，需要技术研究

--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,6 @@
 var Model = require('../model');
+var parse = require('co-body');
+require("babel-core/register");
 var UserController = require('../controller/user.js');
 
 var User = module.exports = function(router) {
@@ -8,18 +10,28 @@ var User = module.exports = function(router) {
 
 User.prototype = {
     register:function() {
-        this.router.get('/user/login', function() {
+        this.router.post('/user/login/:name/:password', function () {
             this.type = 'application/json';
             var params = this.params;
-            //console.log(this.name,this.password);
+            console.log(params);
             var oneUser = UserController(params.name,params.password);
-            if(oneUser.check()){
-                this.body = JSON.stringify({"msg":params.name+" is ok"});
-            }else{
-                this.body = JSON.stringify({"msg":params.name+" is not ok"});
-            }
-        });
+            //todo: 目前该文件有问题，等我晚上回去研究
+            (async function(){
+                var result =  await oneUser.check();
+                console.log('111111111111111111',result);
+                if(result){
+                    this.body = JSON.stringify({"msg":params.name+" is ok"});
+                }else{
+                    this.body = JSON.stringify({"msg":params.name+" is not ok"});
+                }
+            }());
 
+        });
+        //初始化专用，正常情况下不需要暴露出来
+        this.router.get('/user/init',function(){
+            UserController("","").init();
+        });
+        //
         this.router.post('/user/login', function *() {
             var post = (yield parse.form(this));
             this.type = 'application/json';
