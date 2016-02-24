0e3cd2d5a789935a4fc168b54824ee104005934e
CoolGuy <admin@coolguy.pw>
2016-02-24 11:49:54 +0800

    第一个登录协议

--- server/model/user.js
+++ server/model/user.js
@@ -10,37 +10,16 @@ var User = module.exports = function(router) {
 User.prototype = {
     register:function() {
         this.router.post('/user/login/:name/:password', function *() {
-            //this.type = 'application/json';
-            this.type = 'text/plain';
-            var _this = this;
+            this.type = 'application/json';
             var params = this.params;
-            console.log(params);
+            //console.log(params);
             var oneUser = UserController(params.name,params.password);
-
             var getCheckResult = function *(){
                 var check = yield oneUser.check();
                 //console.log("I am the result",check);
                 return R.set(check == 0, "",'');
             };
             this.body = yield getCheckResult();
-            //.then(function(){
-            //    console.log("Success");
-            //    _this.body = JSON.stringify({"msg":params.name+" is ok"});
-            //},function(){
-            //    console.log("Fail");
-            //    _this.body = JSON.stringify({"msg":params.name+" is not ok"});
-            //});
-            //todo: 目前该文件有问题，等我晚上回去研究
-            //(async function(){
-            //    var result =  await oneUser.check();
-            //    console.log('111111111111111111',result);
-            //    if(result){
-            //        this.body = JSON.stringify({"msg":params.name+" is ok"});
-            //    }else{
-            //        this.body = JSON.stringify({"msg":params.name+" is not ok"});
-            //    }
-            //}());
-
         });
         //初始化专用，正常情况下不需要暴露出来
         this.router.get('/user/init',function(){
