8ddf3d5dd6054cefde2781bff178093183370873
CoolGuy <admin@coolguy.pw>
2016-02-24 13:17:22 +0800

    第一个正常的登录协议

--- server/model/user.js
+++ server/model/user.js
@@ -1,5 +1,6 @@
 var Model = require('../model');
 var UserController = require('../controller/user.js');
+var parse = require('co-body');
 var R = require('./../R.js');
 
 var User = module.exports = function(router) {
@@ -9,27 +10,32 @@ var User = module.exports = function(router) {
 
 User.prototype = {
     register:function() {
+        var getCheckResult = function *(oneUser){
+            var check = yield oneUser.check();
+            //console.log("I am the result",check);
+            return R.set(check == 0, "",'');
+        };
+
         this.router.post('/user/login/:name/:password', function *() {
             this.type = 'application/json';
             var params = this.params;
             //console.log(params);
             var oneUser = UserController(params.name,params.password);
-            var getCheckResult = function *(){
-                var check = yield oneUser.check();
-                //console.log("I am the result",check);
-                return R.set(check == 0, "",'');
-            };
-            this.body = yield getCheckResult();
-        });
-        //初始化专用，正常情况下不需要暴露出来
-        this.router.get('/user/init',function(){
-            UserController("","").init();
+
+            this.body = yield getCheckResult(oneUser);
         });
         //
         this.router.post('/user/login', function *() {
             var post = (yield parse.form(this));
             this.type = 'application/json';
-            this.body = JSON.stringify(post);
+            console.log(post);
+            var oneUser = UserController(post.name,post.password);
+            this.body = yield getCheckResult(oneUser);
         });
+        //初始化专用，正常情况下不需要暴露出来
+        this.router.get('/user/init',function(){
+            UserController("","").init();
+        });
+
     }
 };
