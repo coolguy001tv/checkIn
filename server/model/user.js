f2745739a2ab808c7e811b21c33c258ab889876f
lizihan <darkty2009@gmail.com>
2016-02-24 11:17:38 +0800

    async demo

--- server/model/user.js
+++ server/model/user.js
@@ -1,7 +1,7 @@
 var Model = require('../model');
 var parse = require('co-body');
 require("babel-core/register");
-var UserController = require('../controller/user.js');
+//var UserController = require('../controller/user.js');
 
 var User = module.exports = function(router) {
     Model.call(this, router);
@@ -14,20 +14,18 @@ User.prototype = {
             this.type = 'application/json';
             var params = this.params;
             console.log(params);
-            var oneUser = UserController(params.name,params.password);
-            //todo: 目前该文件有问题，等我晚上回去研究
-            (async function(){
-                var result =  await oneUser.check();
-                console.log('111111111111111111',result);
-                if(result){
-                    this.body = JSON.stringify({"msg":params.name+" is ok"});
-                }else{
-                    this.body = JSON.stringify({"msg":params.name+" is not ok"});
-                }
-            }());
+            //var oneUser = UserController(params.name,params.password);
+            //(async function(){
+            //    var result =  await oneUser.check();
+            //    console.log('111111111111111111',result);
+            //    if(result){
+            //        this.body = JSON.stringify({"msg":params.name+" is ok"});
+            //    }else{
+            //        this.body = JSON.stringify({"msg":params.name+" is not ok"});
+            //    }
+            //}());
 
         });
-        //初始化专用，正常情况下不需要暴露出来
         this.router.get('/user/init',function(){
             UserController("","").init();
         });
@@ -36,6 +34,19 @@ User.prototype = {
             var post = (yield parse.form(this));
             this.type = 'application/json';
             this.body = JSON.stringify(post);
+        });
+
+        // async demo
+        var _this = this;
+        this.router.get('/user/async', function *(next) {
+            this.body = (yield _this.asyncDemo());
+        });
+    },
+    asyncDemo:function() {
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                resolve('async is ok');
+            });
         });
     }
 };
